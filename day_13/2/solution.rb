d449a2fe2ca7ab74683ef50126f0f7484dc242e2
Juraj Petrik <jpetrik@represent.com>
2023-02-10 19:31:58 +0100

    Refactor

--- day_13/2/solution.rb
+++ day_13/2/solution.rb
@@ -14,7 +14,7 @@ end
 
 def compare_ints(left_val, right_val)
   return 0 if left_val == right_val # equal value should continue
-  return -1 left_val < right_val # correct order
+  return -1 if left_val < right_val # correct order
   return 1 # incorrect order
 end
 
@@ -23,7 +23,6 @@ def compare_arrays(left_array, right_array)
   return 1 if !left_array.empty? && right_array.empty? # right array run out of items first, incorrect order
   return 0 if left_array.empty? && right_array.empty? # both arrays run out of items, continue checking
   # there are items left in both arrays
-
   left_val = left_array.first
   right_val = right_array.first
 
@@ -47,18 +46,17 @@ def compare_arrays(left_array, right_array)
     raise "Unexpected left_val type #{left_val.class}"
   end
 
-  if comp == -1
+  if comp == 0
     return compare_arrays(left_array.drop(1), right_array.drop(1))
   else
     return comp
   end
 end
 
-sum_of_indices_of_correct_pairs = 0
-pairs.each_with_index do |pair,i|
-  left, right = pair
-  result = compare_arrays(left, right)
-  sum_of_indices_of_correct_pairs += i+1 if result == 1
-end
-
-p "Sum of indices of correct pairs is #{sum_of_indices_of_correct_pairs}"
+divider1 = [[2]]
+divider2 = [[6]]
+pairs.push(divider1)
+pairs.push(divider2)
+sorted = pairs.sort {|left, right| compare_arrays(left,right)}
+key = (sorted.index(divider1)+1) * (sorted.index(divider2) + 1)
+p "key is #{key}"
